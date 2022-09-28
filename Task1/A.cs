6bdb4fd41281e1cd41a8bc8bf69e4194cbd3cc3c
Windawz <46968683+Windawz@users.noreply.github.com>
2022-09-28 13:18:52 +0300

    Finish Task1

---
+++ Task1/A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1;
+class A {
+    private decimal a = 100.58m;
+    private decimal b = 230.75m;
+
+    public decimal C {
+        get {
+            return ++a - b;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Task1;
+class Program {
+    static void Main() {
+        A a = new A();
+        Console.WriteLine("a.C: {0}", a.C);
+    }
+}
