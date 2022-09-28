e31de6df2861caf6318f12ac40cdf532a724b903
Windawz <46968683+Windawz@users.noreply.github.com>
2022-09-28 13:19:06 +0300

    Finish Task2

---
+++ Task2/B.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2;
+class B : A {
+    public B(decimal a, decimal b, decimal d) : base(a, b) {
+        this.d = d;
+    }
+    public B(decimal a, decimal b) : base(a, b) { }
+
+    private decimal d = 12.90m;
+
+    public decimal C2 {
+        get {
+            if (a > 20m || b < 50m || d > 10m && d < 80m) {
+                return a + b + d;
+            }
+            return a - b - d;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2;
+class Program {
+    static void Main() {
+        A a = new A(12.50m, 22.70m);
+        Console.WriteLine("a.C: {0}", a.C);
+
+        B b1 = new B(55.12m, 33.2m);
+        Console.WriteLine("b1.C: {0}", b1.C);
+        Console.WriteLine("b1.C2: {0}", b1.C2);
+
+        B b2 = new B(123.77m, 75.1m, 40m);
+        Console.WriteLine("b2.C: {0}", b2.C);
+        Console.WriteLine("b2.C2: {0}", b2.C2);
+    }
+}
