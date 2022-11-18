191342e7bf3e9e1541885148c5013d8f624309b3
Windawz <46968683+Windawz@users.noreply.github.com>
2022-11-18 13:51:32 +0300

    Finish Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task5;
+
+class Program {
+    static void Main() {
+        var b1 = new B(12, 70);
+        if (b1) {
+            Console.WriteLine("b1 is true");
+        } else {
+            Console.WriteLine("b1 is false");
+        }
+        var b2 = new B(0, 44, 32);
+        if (!b2) {
+            Console.WriteLine("b2 is false");
+        } else {
+            Console.WriteLine("b2 is true");
+        }
+    }
+}
