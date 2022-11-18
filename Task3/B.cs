f15b9279281140e9d908b6f7441a41eb15193995
Windawz <46968683+Windawz@users.noreply.github.com>
2022-11-18 13:50:59 +0300

    Finish Task3

---
+++ Task3/B.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3;
+class B : A {
+    public B(decimal a, decimal b, decimal d) : base(a, b) {
+        this.d = d;
+        Array = new decimal[(int)a];
+        for (int i = 0; i < Array.Length; i++) {
+            Array[i] = C2 * i;
+        }
+    }
+    public B(decimal a, decimal b) : this(a, b, 12.90m) { }
+
+    private decimal d;
+
+    public decimal[] Array { get; }
+    public decimal C2 {
+        get {
+            if (a > 20m || b < 50m || d > 10m && d < 80m) {
+                return a + b + d;
+            }
+            return a - b - d;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3;
+
+class Program {
+    static void Main() {
+        var b = new B(12.80m, 23.00m);
+        foreach (decimal value in b.Array) {
+            Console.WriteLine(value);
+        }
+    }
+}
